039b3605519e7f49afbe8f5d989319ad2b2bbb94
amygdal <32745482+amygdal@users.noreply.github.com>
2018-07-04 17:18:08 -0400

    with some files deleted

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var world = 'the beginning of everything';
-function hello(input) {
-    console.log("you entered: " + input);
-}
-exports.hello = hello;
-//# sourceMappingURL=index.js.map
--- myApp.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function printName(name) {
-    console.log("Hello. The name you entered is: " + name);
-}
-printName('amy');
